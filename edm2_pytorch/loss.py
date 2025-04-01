ecc2e6c5ef75adcea717f507472414e4e4021753
Leeyoungsup <liive29252@gmail.com>
2025-04-01 11:48:34 +0900

    EDM2 손실 함수 수정: SNR 가중치 적용 코드 주석 처리 및 손실 계산 방식 변경; Dockerfile 추가

--- edm2_pytorch/loss.py
+++ edm2_pytorch/loss.py
@@ -8,6 +8,7 @@ def edm2_loss(model, x, sigma, class_labels=None, cfg_drop=0.1, sigma_data=0.5):
     denoised = model(noised, sigma, class_labels)
     snr = (sigma_data / sigma) ** 2
     weight = snr / (snr + 1)
-    loss = (weight[:, None, None, None] * (denoised - noise) ** 2).mean()
+    # loss = (weight[:, None, None, None] * (denoised - noise) ** 2).mean()
+    loss = ((denoised - noise) ** 2).mean()
     return loss
 
